49dc2c39de57ae2f29f59370ceb3787d3ce6c0b4
msuzun <sevki.uzun53@hotmail.com>
2024-12-04 00:29:26 +0300

    flex yapısı eklendi

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,36 @@
-'use client'
+"use client";
 import Pictures from "@/components/Pictures";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [pictures,setPictures] = useState([]);
+  const [pictures, setPictures] = useState([]);
   const [fetchTrigger, setFetchTrigger] = useState(false);
-  const getPicsum = async (url) =>{
+  const getPicsum = async (url) => {
     const response = await fetch(url);
-    if(response.ok){
-      const newImage = {id:crypto.randomUUID(),imageUrl: response.url}
+    if (response.ok) {
+      const newImage = { id: crypto.randomUUID(), imageUrl: response.url };
       setPictures((prevPictures) => [...prevPictures, newImage]);
       setFetchTrigger(false);
     }
-   
-  }
-  useEffect(()=>{
-    if(fetchTrigger){
-      const baseUrl = `https://picsum.photos/seed/${Math.floor(Math.random() * 100)}/200/300`
-      getPicsum(baseUrl)
+  };
+  useEffect(() => {
+    if (fetchTrigger) {
+      const baseUrl = `https://picsum.photos/seed/${Math.floor(
+        Math.random() * 100
+      )}/200/300`;
+      getPicsum(baseUrl);
     }
-  },[fetchTrigger])
-  const handleClick = () =>{
+  }, [fetchTrigger]);
+  const handleClick = () => {
     setFetchTrigger(true);
-  }
+  };
   return (
     <>
       <div>
         <button onClick={handleClick}>Resim Ekle</button>
-        <Pictures pictures = {pictures}/>
+        <div style={{display:'flex',gap:"20px",flexWrap:'wrap',justifyContent:'center'}}>
+          <Pictures pictures={pictures} />
+        </div>
       </div>
     </>
   );
